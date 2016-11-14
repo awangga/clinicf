b30a1e9c34547827c6123120b4c8b3090bfb6e13
Rolly Maulana Awangga <rolly@awang.ga>
2016-11-14 15:24:07 +0700

    add request

--- apps/controllers/input.py
+++ apps/controllers/input.py
@@ -1,14 +1,28 @@
 import sys
 sys.path.append('../../')
 from lib.cilok import urlEncode16,tokenuri,setTTL
+from apps.models.input import *
 
 def ktp(uridt='null'):
+	if isProfileExist(uridt):
+		dt = {
+		'%regform%':'hidden',
+		'%recform%':'visible'
+		}
+	else:
+		dt = {
+		'%regform%':'visible',
+		'%recform%':'hidden'
+		}
+	if uridt=='04123C72862B80':
+		dt['%Name%']='Mila Anisa'
+	elif uridt=='041C2E0A422A80':
+		dt['%Name%']='Rolly Maulana Awangga'
+	else:
+		dt['%Name%']='Ieu lain KTP lur!'
 	thisURI=urlEncode16(tokenuri+'%'+uridt)
 	setTTL(thisURI)
-	dt = {
-	'%UID%':uridt, 
-	'%Nama%':'Rolly Maulana Awangga',
-	'%URI%':thisURI
-	}
+	dt['%URI%']=thisURI
+	dt['%UID%']=uridt
 	return dt
 	
